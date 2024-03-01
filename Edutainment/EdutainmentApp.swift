3963ceebc0c348872b42836f0340bd6ff439c9f8
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-01 08:35:25 -0600

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Jackson Harrison on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
